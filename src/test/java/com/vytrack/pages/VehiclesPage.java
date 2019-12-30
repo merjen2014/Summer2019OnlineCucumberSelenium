ee95ffcd0c972217370498cd5c7a4756233fcca7
merjen2014 <m.rejepowa1988@gmail.com>
2019-12-30 14:28:40 -0800

    new stuff

--- src/test/java/com/vytrack/pages/VehiclesPage.java
+++ src/test/java/com/vytrack/pages/VehiclesPage.java
@@ -1,4 +1,19 @@
 package com.vytrack.pages;
 
-public class VehiclesPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import com.vytrack.utilities.BrowserUtils;
+public class VehiclesPage extends BasePage{
+    @FindBy(css = "[title='Create Car']")
+    public WebElement createACarElement;
+
+    /**
+     * Use this method to click on "Create a Car" button
+     * Method already contains waits to handle synchronization issues
+     */
+    public void clickToCreateACar(){
+        BrowserUtils.waitForVisibility(createACarElement, 15);
+        BrowserUtils.waitForClickablility(createACarElement, 15);
+        createACarElement.click();
+    }
 }
